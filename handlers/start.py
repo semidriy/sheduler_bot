180d19ca9f0e29b0aa968efcdf6bca3b34b65495
Семенов Дмитрий <semidriy@gmail.com>
2025-10-19 11:46:50 +0300

    add spam

--- handlers/start.py
+++ handlers/start.py
@@ -1,16 +1,72 @@
+import asyncio
 from aiogram.filters import Command
 from aiogram import Bot, types, Router, F
 from services.message_scheduler import message_scheduler
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
 
 from functions.get_messages import call_first_start_sales
 
 router = Router()
 
-## Ловим команды /start
+class UserState(StatesGroup):
+    waiting_for_button = State()
+
 @router.message(Command('start'))
-async def admin_command(message: types.Message) -> None:
+async def admin_command(message: types.Message, state: FSMContext) -> None:
+    await state.set_state(UserState.waiting_for_button)
+    await start_periodic_messages(message, state)
+
+async def start_periodic_messages(message: types.Message, state: FSMContext):
+    """Запускает периодические сообщения"""
+    user_id = message.from_user.id
+    
+    # Сохраняем информацию о задаче
+    await state.update_data(periodic_task_active=True)
+    
+    # Отправляем первое сообщение
     await call_first_start_sales(message)
-    await message_scheduler.schedule_welcome_messages(message.from_user.id)
+    
+    # Запускаем периодическую отправку
+    asyncio.create_task(periodic_message_worker(message, state))
+
+async def periodic_message_worker(message: types.Message, state: FSMContext):
+    """Рабочий процесс для периодической отправки сообщений"""
+    user_id = message.from_user.id
+    
+    while True:
+        await asyncio.sleep(10)
+        
+        # Проверяем состояние
+        current_state = await state.get_state()
+        if current_state != UserState.waiting_for_button.state:
+            break
+            
+        # Проверяем флаг в хранилище
+        data = await state.get_data()
+        if not data.get('periodic_task_active', True):
+            break
+            
+        # Отправляем сообщение
+        await call_first_start_sales(message)
+        await message_scheduler.schedule_welcome_messages(message.from_user.id)
+
+# Обработчик нажатия кнопки
+@router.message(UserState.waiting_for_button)
+async def handle_button_press(message: types.Message, state: FSMContext):
+    # Останавливаем периодические сообщения
+    await state.update_data(periodic_task_active=False)
+    await state.clear()
+    
+    # Ваша логика обработки нажатия кнопки
+    button_text = message.text
+    await message.answer(f"Вы нажали: {button_text}. Периодические сообщения остановлены.")
+
+# ## Ловим команды /start
+# @router.message(Command('start'))
+# async def admin_command(message: types.Message) -> None:
+#     await call_first_start_sales(message)
+#     await message_scheduler.schedule_welcome_messages(message.from_user.id)
 
 @router.message(Command('delmenu'))
 async def del_main_menu(message: types.Message, bot: Bot):
